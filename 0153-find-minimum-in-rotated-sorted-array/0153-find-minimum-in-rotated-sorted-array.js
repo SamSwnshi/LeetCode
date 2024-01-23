db88afff9d488a4bf98ad35ab7b6962223043ab1
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-01-23 15:24:55 +0530

    Time: 50 ms (71.48%), Space: 49.1 MB (5.01%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let n = nums.length;
+    nums.sort((a,b)=> a -b )
+    let left = 0 ;
+    let right = n - 1;
+    while(left < right){
+        let mid = Math.floor((left+ right)/2)
+        if(nums[mid] > nums[right]){
+            left = mid + 1;
+            
+        }else{
+            right = mid
+        }
+    }
+    return nums[left]
+};

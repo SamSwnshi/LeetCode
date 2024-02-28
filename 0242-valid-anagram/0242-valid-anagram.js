f2612662050045b3d5e73144dfba1f7734cf8006
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-02-28 22:53:03 +0530

    Time: 71 ms (63.17%), Space: 51.2 MB (49.40%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,15 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    if(s.length !== t.length) return false;
-    const char = Array(26).fill(0)
-    for(let i = 0 ;i < s.length; i++){
-        char[s.charCodeAt(i) - 'a'.charCodeAt(0)]++;
-        char[t.charCodeAt(i) - 'a'.charCodeAt(0)]--;
+    if(s.length !== t.length ) return false;
+  let n = new Map()
+  for(let i = 0 ; i < s.length; i++){
+      let c = s.charAt(i)
+      if(n.has(c)){
+          n.set(c,n.get(c) + 1)
+      }else{
+          n.set(c,1)
+      }
+  }
+    for(let i = 0 ; i < t.length;i++){
+        let c = t.charAt(i)
+        if(n.has(c)){
+            n.set(c,n.get(c) - 1)
+        }else {
+            return false
+        }
     }
-    for(let count of char){
-        if(count !== 0){
-        return false
+    
+    for(let val of n.values()){
+        if(val != 0){
+            return false
         }
     }
     return true;

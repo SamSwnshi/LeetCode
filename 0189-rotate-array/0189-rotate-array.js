ce2b41aad14920883df7b61eef9a7f927221e9a1
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-02-28 23:22:18 +0530

    Time: 75 ms (82.58%), Space: 57.9 MB (32.64%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.js
+++ 0189-rotate-array/0189-rotate-array.js
@@ -4,21 +4,18 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    let n = nums.length;
-    k = k % n ;
-    reverse(nums,0,n-1)
-    reverse(nums,0,k-1)
-    reverse(nums,k,n-1)
-    return nums;
-    
-}
-
+    k = k % nums.length;
+    reverse(nums,0,nums.length -1)
+    reverse(nums,0,k - 1)
+    reverse(nums,k ,nums.length - 1)
+    return nums
+};
 function reverse(nums,start,end){
     while(start < end){
         let temp = nums[start]
         nums[start] = nums[end]
         nums[end] = temp
-        start++;
-        end--;
+        start++
+end--
     }
 }

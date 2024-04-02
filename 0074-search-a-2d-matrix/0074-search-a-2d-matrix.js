76e663769623e78d6544e1fda889940f405a857f
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-04-02 22:10:52 +0530

    Time: 47 ms (83.50%), Space: 49.3 MB (19.06%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -7,21 +7,20 @@ var searchMatrix = function(matrix, target) {
     let m = matrix.length;
     let n = matrix[0].length;
     
-    let left = 0,right = m * n - 1;
-    
-    while(left <= right){
+    let left = 0;
+    let right = m * n - 1;
+    while(left <= right ){
         let mid = Math.floor((left+ right)/2)
-        let midEle = matrix[Math.floor(mid/n)][mid%n]
         
+        let midEle = matrix[Math.floor(mid/n)][mid % n]
         if(midEle === target){
-            return true
+return true
         }else if(midEle < target){
-            left = mid + 1
-        }else{
-           right = mid - 1
+                 left = mid + 1;
+                 }else{
+            right = mid - 1;
         }
-        
-            
     }
-    return false;
+    
+    return false
 };

5cfff5e836d287ad14871dbf9f6d7e0c95e666b1
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-04-03 22:26:12 +0530

    Time: 1073 ms (11.83%), Space: 76.8 MB (6.25%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,24 +2,25 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-function merge(left,right){
-    let sorted = [];
-    while(left.length && right.length){
-        if(left[0] < right[0]){
-            sorted.push(left.shift())
-        }else{
-            sorted.push(right.shift())
-        }
+function merger(left,right){
+let sorted = []
+while(left.length && right.length){
+    if(left[0] < right[0]){
+        sorted.push(left.shift())
+    }else{
+        sorted.push(right.shift())
     }
-    return [...sorted,...left,...right];
-    }
+}
+    return [...sorted,...left,...right]
+}
 function mergeSort(nums){
-    if (nums.length <= 1) return nums
-  let mid = Math.floor(nums.length / 2)
-  // Recursive calls
-  let left = mergeSort(nums.slice(0, mid))
-  let right = mergeSort(nums.slice(mid))
-  return merge(left, right)
+    if(nums.length === 1 )return nums;
+    
+    let mid = Math.floor(nums.length/2);
+    
+    let left = mergeSort(nums.slice(0,mid))
+    let right = mergeSort(nums.slice(mid))
+    return merger(left,right)
 }
 var sortArray = function(nums) {
     return mergeSort(nums)

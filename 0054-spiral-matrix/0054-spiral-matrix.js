b1c2e107d880199ab5c9a0b4050e65c36bf64edc
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-04-04 16:40:16 +0530

    Time: 44 ms (89.35%), Space: 48.6 MB (74.38%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -3,43 +3,35 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-     if(matrix.length === 0 || matrix[0].length === 0){
-        return []
-    }
-    const result = [];
-    let top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
-
-    while (top <= bottom && left <= right) {
-        // Traverse top row
-        for (let i = left; i <= right; i++) {
-            result.push(matrix[top][i]);
+    const mat  = []
+    let top = 0; 
+    let bottom = matrix.length - 1;
+    let left = 0;
+    let right = matrix[0].length - 1;
+    
+    while(top <= bottom && left <= right){
+        for(let i = left ;i <= right;i++){
+            mat.push(matrix[top][i])
         }
         top++;
-
-        // Traverse right column
-        for (let i = top; i <= bottom; i++) {
-            result.push(matrix[i][right]);
+        for(let i = top ; i <= bottom; i++){
+            mat.push(matrix[i][right])
         }
         right--;
-
-        // Traverse bottom row
-        if (top <= bottom) {
-            for (let i = right; i >= left; i--) {
-                result.push(matrix[bottom][i]);
-            }
-            bottom--;
+        
+        if(top <= bottom){
+        for(let i = right; i >= left;i--){
+            mat.push(matrix[bottom][i])
         }
-
-        // Traverse left column
-        if (left <= right) {
-            for (let i = bottom; i >= top; i--) {
-                result.push(matrix[i][left]);
-            }
-            left++;
+        bottom--;
+        }
+        if(left  <= right){
+        for(let i = bottom; i >= top;i-- ){
+            mat.push(matrix[i][left])
+        }
+        left++
         }
     }
-
-    return result;
-    
+    return mat;
     
 };

3a044f3c3e2b068f11ab81ff7168564e079f2ba7
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-05-26 22:50:55 +0530

    Time: 55 ms (82.96%), Space: 51.3 MB (28.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let n = new Map();
-    
-    for(let i = 0; i < nums.length;i++){
-        if(n.has(target-nums[i])){
-        return [n.get(target-nums[i]),i]
+   
+    let n = new Map()
+    for(let i = 0; i < nums.length ; i++){
+        if(n.has(target - nums[i]) ){
+            return [n.get(target - nums[i]),i]
         }else{
             n.set(nums[i],i)
         }

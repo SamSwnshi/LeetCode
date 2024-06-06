9966fd50d253d44e53fffab9a977550b001ad158
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-06-06 20:41:42 +0530

    Time: 51 ms (87.81%), Space: 49.9 MB (88.18%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let count = 0;
+    for(let i = 0 ; i < nums.length; i++){
+        count ^= nums[i]
+        
+    }
+    return count;
+};

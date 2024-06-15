774837ad92b0d475c4fa84acb94de83151d59b8f
Sameer Suryawanshi <114623580+SamSwnshi@users.noreply.github.com>
2024-06-15 22:53:02 +0530

    Time: 144 ms (91.33%), Space: 65.9 MB (30.44%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,7 +3,7 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-       const res=[]
+      const res=[]
     nums.sort((a,b)=>a-b)
     for(let i=0;i<nums.length;i++){
         if(i>0 && nums[i] == nums[i-1]){
